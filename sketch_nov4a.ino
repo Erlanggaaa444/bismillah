dbbec84afc0caf5897a9a3a98015304d8732fafd
aniszaaa <149872712+aniszaaa@users.noreply.github.com>
2023-11-07 17:59:18 +0700

    Update sketch_nov4a.ino

--- sketch_nov4a.ino
+++ sketch_nov4a.ino
@@ -1,14 +1,25 @@
-#include <multi_channel_relay.h>
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-#include <Key.h>
-#include <Keypad.h>
 
-void setup() {
-  // put your setup code here, to run once:
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(1,0);
+  lcd.print("TAMPILKAN LCD MODE");
+  lcd.setCursor(2,0);
+ lcd.print("DAN CENSOR VALLUE");
+   
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop()
+{
 }
